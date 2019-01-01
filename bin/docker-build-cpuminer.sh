8a1aacb635f4479c7b0b7bff5c8c0b715316514a
ventaquil <konrad.golawski@gmail.com>
2019-01-01 18:27:13 +0100

    Autobuild Argon2 image when needed

--- bin/docker-build-cpuminer.sh
+++ bin/docker-build-cpuminer.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
+
+docker image inspect watcoin:argon2 &>/dev/null
+if [ "$?" != "0" ]
+then
+    dir=$(dirname $(realpath "$0"))
+    $(echo "$dir/docker-build-argon2.sh")
+fi
+
 docker-compose build \
     cpuminer
